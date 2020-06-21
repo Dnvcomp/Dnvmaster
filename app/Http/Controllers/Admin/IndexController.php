d7e354ddac67d893ef6942e19bea01a0c983ad67
Dnvcomp <dnvcomp@hotmail.com>
2020-06-21 23:23:50 +0300

    Rights verification method

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -6,12 +6,16 @@ use Illuminate\Http\Request;
 
 use Dnvmaster\Http\Requests;
 use Dnvmaster\Http\Controllers\Controller;
+use Gate;
 
 class IndexController extends AdminController
 {
     public function __construct()
     {
        parent::__construct();
+       if (Gate::denies('VIEW_ADMIN')) {
+           abort(403);
+       }
        $this->template = env('MASTER').'.admin.index';
     }
 
